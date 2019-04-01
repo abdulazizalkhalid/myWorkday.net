f61baa28391300b522d514b32a4c5ba7adee3311
akhalid <akhalid@DSP-54>
2019-03-31 21:42:28 -0400

    Adding initial logic for the method CalculateWorkday and first test case.

--- Interview.Test/WorkdayTests.cs
+++ Interview.Test/WorkdayTests.cs
@@ -6,9 +6,19 @@ namespace Interview.Test
 	[TestClass]
 	public class WorkdayTests
 	{
-		[TestMethod]
-		public void TestMethod1()
-		{
-		}
-	}
+        /// <summary>
+        /// This method tests the calculation for a short period during a single week work days.
+        /// The start date and the calculated date do not reach the weekends.
+        /// In other words, the whole calculation is done over the main body days of the week (middle).
+        /// </summary>
+        [TestMethod]
+        public void TestMethodShortMiddleOfTheWeek()
+        {
+            var myDay = 2;
+            var myDate = DateTime.Parse("13-March-2018");
+            var actual = Interview.Workday.CalculateWorkday(myDate, myDay);
+            var expected = DateTime.Parse("15-March-2018");
+            Assert.AreEqual(expected, actual);
+        }
+    }
 }
--- Interview/Workday.cs
+++ Interview/Workday.cs
@@ -8,10 +8,31 @@ namespace Interview
 {
     public class Workday
     {
+        /// <summary>
+        /// Calculate the number of working days.
+        /// </summary>
+        /// <param name="startDate">The start date from which the calculation will be started</param>
+        /// <param name="numberOfWOrkdays">The number of work days need to be added to the start date </param>
+        /// <returns>The work day that we get after adding the number of workdays to the start date</returns>
         public static DateTime CalculateWorkday(DateTime startDate, int numberOfWOrkdays)
         {
-            // TODO: test and implement me!
-            return DateTime.Today;
+            DateTime myStartDate = startDate;
+            // a local variable to hold the start date. We can work on the same argument of the function but my preference to define local variables.
+            DateTime myUpdatedStartDate = myStartDate;
+
+            /* a local variable to hold the value that the function will return. 
+            As starting point, we assign to it the same value entered by the user for the start date.*/
+            DateTime myCalulatedDate = myStartDate;
+
+            /* a local variable to hold the number of working days. 
+            We can use the same argument of the function (in the header of the function) but my preference to define a local variable.*/
+            int myNumberOfWorkDays = numberOfWOrkdays;
+
+            // We add the  days to the updated date and assign the output to the variable which will contain the calculated value.
+            myCalulatedDate = myUpdatedStartDate.AddDays(myNumberOfWorkDays);
+
+            // we return the calculated date.
+            return myCalulatedDate;
         }
     }
 
